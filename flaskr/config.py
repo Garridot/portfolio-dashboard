c2e473dc56529b478c5f653fc25f138c4624470b
Tomas Garrido <garridot210@gmail.com>
2024-08-09 22:07:57 -0300

    Handle missing ALLOWED_ORIGINS in CORS setup.

--- flaskr/config.py
+++ flaskr/config.py
@@ -24,6 +24,7 @@ class ProductionConfig(Config):
     DEBUG = False
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
+    ALLOWED_ORIGINS = os.environ.get('ALLOWED_ORIGINS')
 
 class TestConfig(Config):
     TESTING = True
